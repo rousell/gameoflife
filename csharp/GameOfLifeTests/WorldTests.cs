5b8dfa21e17fc80122564d81bca1b5da4fe91284
rousell <lauren.l.rouse@gmail.com>
2016-04-18 18:33:04 -0500

    unit tests refactored, test with cycle working

--- csharp/GameOfLifeTests/WorldTests.cs
+++ csharp/GameOfLifeTests/WorldTests.cs
@@ -7,59 +7,69 @@ using GameOfLife;
 
 namespace GameOfLifeTests
 {
-    /*[TestClass]
+    [TestClass]
     public class WorldTests
     {
         [TestMethod]
         public void WorldClassIsNotNull()
         {
-            World Game = new World();
+            World Game = new World(45);
             Assert.IsNotNull(Game);
         }
         [TestMethod]
         public void GameGridBoundTest()
         {
-            World Game = new World();
-            Game.PrintGameGrid();
+            World Game = new World(45);
+            GameLogic Logic = new GameLogic();
+            Logic.PrintGameGrid(Game);
 
-            Assert.AreEqual(0, World.gameGrid[44, 44]);
+            Assert.AreEqual(0, Game.gameGrid[44, 44]);
         }
         [TestMethod]
         [ExpectedException(typeof(IndexOutOfRangeException))]
         public void GameGridIndexOutOfRange()
         {
-            World Game = new World();
-            Game.PrintGameGrid();
+            World Game = new World(45);
+            GameLogic Logic = new GameLogic();
+            Logic.PrintGameGrid(Game);
 
-            Assert.AreNotEqual(0, World.gameGrid[49, 44]);
+            Assert.AreNotEqual(0, Game.gameGrid[49, 44]);
         }
         [TestMethod]
         public void GameGridSeededBlock()
         {
-            World Game = new World();
-            Game.Seed("B");
-            Game.PrintGameGrid();
+            World Game = new World(45);
+            GameLogic Logic = new GameLogic();
+            Seed Seeded = new Seed();
+            World BlockGame = Seeded.Block(Game);
+            Logic.PrintGameGrid(BlockGame);
 
-            Assert.AreEqual(1, World.gameGrid[10, 10]);
+            Assert.AreEqual(1, BlockGame.gameGrid[10, 10]);
         }
         [TestMethod]
         public void GameGridSeededGlider()
         {
-            World Game = new World();
-            Game.Seed("G");
-            Game.PrintGameGrid();
+            World Game = new World(45);
+            GameLogic Logic = new GameLogic();
+            Seed Seeded = new Seed();
+            World GliderGame = Seeded.Glider(Game);
+            Logic.PrintGameGrid(GliderGame);
 
-            Assert.AreEqual(1, World.gameGrid[11, 11]);
+            Assert.AreEqual(1, GliderGame.gameGrid[11, 11]);
         }
         [TestMethod]
         public void SecondIterationBlinker()
         {
-            World Game = new World();
-            Game.Seed("L");
-            Game.PrintGameGrid();
-            Game.Cycle();
+            World Game = new World(45);
+            GameLogic Logic = new GameLogic();
+            Seed Seeded = new Seed();
+            World BlinkerGame = Seeded.Blinker(Game);
+            Logic.PrintGameGrid(BlinkerGame);
+            Logic.Cycle(BlinkerGame);
+            Logic.UpdateGrid(BlinkerGame);
+            Logic.PrintGameGrid(BlinkerGame);
 
             Assert.AreEqual(1, Game.gameGrid[10, 10]);
-        }*/
-    //}
+        }
+    }
 }
